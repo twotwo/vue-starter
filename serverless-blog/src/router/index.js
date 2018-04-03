c778e9102c371a8626ed7ac18c9387b7dd43d048
twotwo <twotwo.li@gmail.com>
2018-04-03 21:14:00 +0800

    调教eslint

--- serverless-blog/src/router/index.js
+++ serverless-blog/src/router/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import BlogHome from "@/components/BlogHome";
+import BlogPost from "@/components/BlogPost";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/blog/",
+      name: "blog-home",
+      component: BlogHome
+    },
+    {
+      path: "/blog/:slug",
+      name: "blog-post",
+      component: BlogPost
     }
   ]
-})
+});
