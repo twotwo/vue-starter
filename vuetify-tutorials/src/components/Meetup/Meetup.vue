fbed7f5bc04004f11fb517fada241c40ac8dbb1d
twotwo <twotwo.li@gmail.com>
2018-04-20 21:26:44 +0800

    6. 聚会列表及详情

---
+++ vuetify-tutorials/src/components/Meetup/Meetup.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title>
+            <h6 class="primary--text">My Meetup</h6>
+          </v-card-title>
+          <v-card-media
+            height="400px"
+            src="http://ryugaku.myedu.jp/images/chn/sc/pekin.jpg"
+            ></v-card-media>
+          <v-card-text>
+            <div class="info--text">2018年8月8日 - 北京大学</div>
+            <div>聚会详情 ~~~~~~~~~~~~~~~~~~~~~~~~~</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="primary">Register</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
