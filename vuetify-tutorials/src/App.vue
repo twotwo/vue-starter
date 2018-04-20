1a784c3efdef653415dbeb47458092da7359e485
twotwo <twotwo.li@gmail.com>
2018-04-20 18:29:39 +0800

    版本升级 npm i vuetify vue vue-router --save

--- vuetify-tutorials/src/App.vue
+++ vuetify-tutorials/src/App.vue
@@ -15,7 +15,6 @@
         flat 
         v-for="item in menuItems"
         :key="item.title"
-        router
         :to="item.link">
           <v-icon left>{{ item.icon }}</v-icon>
           {{ item.title }}
@@ -33,7 +32,6 @@
         <v-list-tile 
         v-for="item in menuItems" 
         :key="item.title"
-        router
         :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
--- vuetify-tutorials/src/components/Home.vue
+++ vuetify-tutorials/src/components/Home.vue
@@ -2,10 +2,10 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
-        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+        <v-btn large to="/meetups" class="info">Explore Meetups</v-btn>
       </v-flex>
       <v-flex xs12 sm6 class="text-xs-center text-sm-left">
-        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+        <v-btn large to="/meetup/new" class="info">Organize Meetup</v-btn>
       </v-flex>
     </v-layout>
     <v-layout row wrap class="mt-2">
