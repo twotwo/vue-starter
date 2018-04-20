7f1c7a3d14f7f9d4d4bf52ed872cbb20734bf3f3
twotwo <twotwo.li@gmail.com>
2018-04-20 11:35:37 +0800

    format

--- serverless-blog/src/components/CommonHeader.vue
+++ serverless-blog/src/components/CommonHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <div style="display: flex; justify-content: flex-end">
     <div class="text-xs-center">
-      <v-chip outline color="green" text-color="white"><router-link to="/login">Login</router-link></v-chip>
+      <v-chip outline color="green" text-color="white">
+        <router-link to="/login">Login</router-link>
+      </v-chip>
 
       <v-chip outline color="green" text-color="white">
         <router-link to="/info">User Info</router-link>
