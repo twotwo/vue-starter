459262fedb851ab9f40d992bca04c6d81efb9511
twotwo <twotwo.li@gmail.com>
2018-04-20 11:06:32 +0800

    4.2 添加页面

--- serverless-blog/src/components/CommonNav.vue
+++ serverless-blog/src/components/CommonNav.vue
@@ -1,7 +1,7 @@
 <template>
   <v-tabs align-with-title right color="cyan">
     <v-tabs-slider color="yellow"></v-tabs-slider>
-    <div><v-btn color="info" round href="#">Home</v-btn></div>
+    <div><v-btn color="info" round href="#/">Home</v-btn></div>
     <div><v-btn color="info" round href="#/test">Test Vue.js</v-btn></div>
     <div><v-btn color="info" round href="#/customers">Customers</v-btn></div>
     <div><v-btn color="info" round href="#/blog">Blog Posts</v-btn></div>
--- vuetify-tutorials/src/router.js
+++ vuetify-tutorials/src/router.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Meetups from '@/components/Meetup/Meetups.vue'
+import CreateMeetup from '@/components/Meetup/CreateMeetup.vue'
+import Profile from '@/components/User/Profile.vue'
+import Signin from '@/components/User/Signin.vue'
+import Signup from '@/components/User/Signup.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  // mode: 'history',
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/meetups',
+      name: 'Meetups',
+      component: Meetups
+    },
+    {
+      path: '/meetup/new',
+      name: 'CreateMeetup',
+      component: CreateMeetup
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     }
   ]
 })
